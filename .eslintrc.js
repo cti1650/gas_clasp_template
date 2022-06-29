4c3f97d0cd07c39cfc964f7ed54acc93a51c2a13
cti1650 <cti1650@gmail.com>
2022-06-30 00:13:46 +0900

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    "googleappsscript/googleappsscript": true,
+  },
+  plugins: ["googleappsscript"],
+  extends: ["airbnb-base", "eslint:recommended", "prettier"],
+  parserOptions: {
+    allowForLoopAfterthoughts: true,
+    ecmaVersion: "latest",
+    properties: "always",
+    ignoreGlobals: true,
+    ignoreDestructuring: true,
+    ignoreImports: true,
+    "no-use-before-define": [
+      "error",
+      { functions: true, classes: true, variables: true },
+    ],
+  },
+  rules: {},
+};
